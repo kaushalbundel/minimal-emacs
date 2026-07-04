23e9bfa58f5795b87b81b23222fe0f5c530b3f97
Kaushal Bundel <kaushalbundel@gmail.com>
2026-07-05 02:37:13 +0530

    vim emulation added

--- init.el
+++ init.el
@@ -407,9 +407,6 @@
   :config
   (setq wgrep-auto-save-buffer t))
 
-(use-package surround
-  :bind-keymap ("M-'" . surround-keymap))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -684,3 +681,59 @@
      indent-bars-color-by-depth '(:regexp "outline-\\([0-9]+\\)" :blend 1)
      indent-bars-highlight-current-depth '(:blend 0.5)
      indent-bars-display-on-blank-lines nil))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Vim emulation
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-keybinding nil)
+  (setq evil-respect-visual-line-mode t)
+  (setq evil-undo-system 'undo-redo)
+  ;; Enable this if you want C-u to scroll up, more like pure Vim
+  ;(setq evil-want-C-u-scroll t)
+  :config
+  (evil-mode)
+  ;; Configuring initial major mode for some modes
+  (evil-set-initial-state 'vterm-mode 'emacs))
+
+(eval-when-compile
+  ;; It has to be defined before evil-colllection
+  (setq evil-collection-setup-minibuffer t))
+
+;; vast collection of vim related keybindings (https://github.com/emacs-evil/evil-collection) should be studied
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+;; surrounding keys
+(use-package evil-surround
+  :after evil
+  :ensure t
+  :commands global-evil-surround-mode
+  :custom
+  (evil-surround-pairs-alist
+   '((?\( . ("(" . ")"))
+     (?\[ . ("[" . "]"))
+     (?\{ . ("{" . "}"))
+
+     (?\) . ("(" . ")"))
+     (?\] . ("[" . "]"))
+     (?\} . ("{" . "}"))
+
+     (?< . ("<" . ">"))
+     (?> . ("<" . ">"))))
+  :hook (after-init . global-evil-surround-mode))
+
+;; commenting out code
+(use-package evil-commentary
+  :after evil
+  :ensure t
+  :config
+  (evil-commentary-mode))
