90235d5a7c80345bf3e4003aedd6eb0d8af1d822
Kaushal Bundel <kaushalbundel@gmail.com>
2026-07-05 02:43:50 +0530

    line number is set to relative

--- init.el
+++ init.el
@@ -200,7 +200,7 @@
                 elfeed-show-mode-hook
                 elfeed-search-mode-hook))
   (add-hook mode (lambda ()(display-line-numbers-mode 0))))
-(setopt display-line-numbers-type t)
+(setopt display-line-numbers-type 'relative)
 
 (add-hook 'text-mode-hook 'visual-line-mode)
 (dolist (hook '(text-mode-hook prog-mode-hook))
@@ -412,7 +412,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(apheleia auto-package-update avy cape consult-denote consult-eglot
+              corfu-terminal crux denote-journal doom-modeline
+              embark-consult emmet-mode evil evil-collection
+              evil-commentary evil-surround exec-path-from-shell
+              highlight-indent-guides json-mode kind-icon magit
+              marginalia markdown-mode modus-themes
+              nerd-icons-completion nerd-icons-dired orderless
+              org-modern rainbow-delimiters rainbow-mode
+              spacious-padding surround swift-mode uv-mode vertico
+              wgrep yaml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
